f530b8d6b793087f04a96156191cb282e9d51c46
Pc Chou <pcchou@pcchou.me>
2015-02-05 19:05:30 +0800

    use constant

--- config.sample.php
+++ config.sample.php
@@ -14,7 +14,7 @@
  * 此為本程式所在站點的網址
  * 請在開頭加上 http:// 字串尾巴要有 /
 */
-$newURL = 'http://site/';
+define("newURL",'http://site/');
 
 /*
  * $json
@@ -22,12 +22,12 @@ $newURL = 'http://site/';
  * 請在開頭加上 /
  * 請一併更改 redirect.php 的相同一行
 */
-$json = '/k8agJa1__.json';
+define("json",'/k8agJa1__.json');
 
 /*
  * $regenerate_config
  * true : 可以透過 http://site/index.php?action=regenerate_config 來重置設定
  * false : 不可以重置設定（建議值）
 */
-$regenerate_config = true;
+define("regenerate_config",true);
 
