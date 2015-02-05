14863f468e8801b7063322310eeea7ed9ce6ae27
Pc Chou <pcchou@pcchou.me>
2015-02-05 19:00:44 +0800

    added config.sample.php

--- index.php
+++ index.php
@@ -9,33 +9,13 @@
 header("Content-type: text/html; charset=utf-8");
 
 /*  必讀
- *	第一次使用，請務必訪問一次 
+ *	第一次使用，請務必訪問一次
  *  http://站點網址/index.php?action=regenerate_config
- *  來生成資料庫檔案，然後將下面的 $regenerate_config = true;
+ *  來生成資料庫檔案，然後將 config.sample.php 的 $regenerate_config = true;
  *  改成 $regenerate_config = false;
+ *  並且完成其他設定後，改名成 config.php
 */
-//---------------- 設定 ---------------//
-/*
- * $newURL
- * 此為本程式所在站點的網址
- * 請在開頭加上 http:// 字串尾巴要有 /
-*/
-$newURL = 'http://site/';
-
-/*
- * $json
- * 此為短網址資料庫存放位置
- * 請在開頭加上 /
- * 請一併更改 redirect.php 的相同一行
-*/
-$json = '/k8agJa1__.json';
-
-/*
- * $regenerate_config
- * true : 可以透過 http://site/index.php?action=regenerate_config 來重置設定
- * false : 不可以重置設定（建議值）
-*/
-$regenerate_config = true;
+include "config.php";
 
 //---------------- 請勿更改 ---------------//
 $json = dirname(__FILE__) . $json;
@@ -60,18 +40,18 @@ if($newURL == 'http://site/'){
 
 if(isset($_POST['action']) and $_POST['action'] == 'generate'){
 	if(	isset($_POST['url']) and
-	stripos($_POST['url'], 'http') !== FALSE and 
-	stripos($_POST['url'], ':') !== FALSE and 
-	stripos($_POST['url'], '//') !== FALSE and 
-	stripos($_POST['url'], '.') !== FALSE and 
-	stripos($_POST['url'], '\r') === FALSE and 
-	stripos($_POST['url'], '\n') === FALSE and 
-	stripos($_POST['url'], '%00') === FALSE and 
-	stripos($_POST['url'], '"') === FALSE and 
-	stripos($_POST['url'], '\'') === FALSE and 
-	stripos($_POST['url'], '{') === FALSE and 
+	stripos($_POST['url'], 'http') !== FALSE and
+	stripos($_POST['url'], ':') !== FALSE and
+	stripos($_POST['url'], '//') !== FALSE and
+	stripos($_POST['url'], '.') !== FALSE and
+	stripos($_POST['url'], '\r') === FALSE and
+	stripos($_POST['url'], '\n') === FALSE and
+	stripos($_POST['url'], '%00') === FALSE and
+	stripos($_POST['url'], '"') === FALSE and
+	stripos($_POST['url'], '\'') === FALSE and
+	stripos($_POST['url'], '{') === FALSE and
 	stripos($_POST['url'], '}') === FALSE){
-		
+
 		$done = false;
 		$url = $_POST['url'];
 		$valueADD = ' value="' . $_POST['url'] . '"';
