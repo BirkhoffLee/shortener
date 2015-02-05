f3b2435faa0c305fc5b01bd9f375b1300af2e63a
Birkhoff Lee <b@irkhoff.com>
2015-02-05 19:59:35 +0800

    Doing some changes on Pc Chou's new version

--- redirect.php
+++ redirect.php
@@ -1,14 +1,7 @@
 <?php
-/*
- * $json
- * 此為短網址資料庫存放位置
- * 請在開頭加上 /
- * 請一併更改 index.php 的相同一行
-*/
 require_once "config.php" or die('<h1>Cannot access config file. Did you read the message in index.php?</h1>');
 global $json;
 
-//---------------- 請勿更改 ---------------//
 if(strlen($_GET['id']) == 0){
 	header('Location: /index.php');
 	exit;
